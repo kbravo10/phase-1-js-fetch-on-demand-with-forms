b99eb4e1540e1232ccb1175d6accf7096960ba6d
Kevin Bravo <kbravo1024@gmail.com>
2023-04-20 08:50:39 -0700

    complete fetchOnDemand lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e);
+    
+    const userInput = document.getElementById("searchByID")
+    console.log(userInput.value)
+
+    fetch(`http://localhost:3000/movies/${userInput.value}`)
+    .then((res) => res.json())
+    .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    })
+}
+)
 }
 
 document.addEventListener('DOMContentLoaded', init);
